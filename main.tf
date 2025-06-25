d97c2ad6ffb06100e92c12a60cd4ea2745370492
GaFer <89945798+GaryFernandoYM@users.noreply.github.com>
2025-06-24 20:49:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,48 +7,18 @@ provider "aws" {
 # VPC existente
 data "aws_vpc" "default" {}
 
-# 🔐 Security Group (aquí puedes mantenerlo si deseas recrear)
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2_s3_sg_fegf"
-  description = "Permitir SSH, HTTP y HTTPS"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "SSH desde cualquier lugar"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+data "aws_security_group" "existing_sg" {
+  filter {
+    name   = "group-name"
+    values = ["ec2_s3_sg_fegf"]
   }
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Todo el trafico de salida"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "sg_ec2_s3_fegf"
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
+
 
 # ✅ Reutilizar Role existente (no crear)
 data "aws_iam_role" "existing_role" {
